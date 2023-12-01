6a3141aee3704305eb3afe4c10dd07cf009d4580
Jack Gardner <shortfilmsbyjack@aol.com>
2023-11-30 19:18:08 -0800

    fixxed errors in modelone
    fixxed errors with modelone having to do with improper implementation of vosk and wav files

--- ModelOne.py
+++ ModelOne.py
@@ -1,11 +1,46 @@
-import vosk
+from vosk import Model, KaldiRecognizer
+from queue import Queue 
+import wave
 
+def model_one(input_audio_Q, output_text_queue, com_in, com_out):
+    try:
+        model = Model("vosk-model-small-en-us-0.15")
+        print(1)
 
-def model_one(input_audio,output_text_queue, error_queue):
-    # runs for a finite ammount of time before 
-    model_path = "voskModel1"
-    recognizer = vosk.Model(model_path)
+        audio = input_audio_Q.get()
+        print(2)
 
-    result: str = recognizer.recognize(input_audio)
+        data = audio.readframes(audio.getnframes())
+        print(3)
 
-    output_text_queue.put(result)
+        recognizer = KaldiRecognizer(model, audio.getframerate())
+        recognizer.AcceptWaveform(data)  # Accept the entire waveform
+        print(4)
+
+        result = recognizer.Result()
+        print(5)
+
+        output_text_queue.put(result)
+        print(6)
+
+    except Exception as e:
+        print(f"Error in model_one: {e}")
+
+if __name__ == "__main__":
+    com_inQ = Queue()
+    com_outQ = Queue()
+    outputQ = Queue()
+
+    audio = wave.open("testAudio.wav", 'rb')  # Note the 'rb' mode to open the WAV file in binary mode
+    audioQ = Queue()
+    audioQ.put(audio)
+    print(audioQ)
+
+    try:
+        model_one(audioQ, outputQ, com_inQ, com_outQ)
+    except Exception as e:
+        print(f"Error in __main__: {e}")
+
+    # Retrieve and print the content of outputQ
+    while not outputQ.empty():
+        print("Content of outputQ:", outputQ.get())
