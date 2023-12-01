9f939cef16a566f907fa44edf6d0e9a4c0987f79
cromeros <cromerosanchez@wsu.edu'clear>
2023-11-30 19:34:38 -0800

    Working Audio Listener

--- AudioListener.py
+++ AudioListener.py
@@ -4,37 +4,46 @@ from vosk import Model, KaldiRecognizer
 import sys
 import json
 
-def audio_listener(plane_id: str, audio_segment_queue: queue, comm_in_queue: queue, comm_out_queue: queue):
-    device_info = sd.query_devices(sd.default.device[0], 'input')
-    samplerate = int(device_info['default_samplerate'])
-    q = queue.Queue()
+'''This script processes audio input from the microphone and displays the transcribed text.'''
+# get the samplerate - this is needed by the Kaldi recognizer
+device_info = sd.query_devices(sd.default.device[0], 'input')
+samplerate = int(device_info['default_samplerate'])
 
-    def recordCallback(indata, frames, time, status):
-        if status:
-            print(status, file=sys.stderr)
-        q.put(bytes(indata))
-    model = Model("voskModel1")
-    recognizer = KaldiRecognizer(model, samplerate)
-    recognizer.SetWords(False)
+# display the default input device
+print("===> Initial Default Device Number:{} Description: {}".format(sd.default.device[0], device_info))
 
-    print("===> Begin recording. Press Ctrl+C to stop the recording ")
-    try:
-        with sd.RawInputStream(dtype='int16',
-                            channels=1,
+# setup queue and callback function
+q = queue.Queue()
 
-                            callback=recordCallback):
-            while True:
-                data = q.get()        
-                if recognizer.AcceptWaveform(data):
-                    recognizerResult = recognizer.Result()
-                    # convert the recognizerResult string into a dictionary  
-                    resultDict = json.loads(recognizerResult)
-                    if not resultDict.get("text", "") == "":
-                        print(recognizerResult)
-                    else:
-                        print("no input sound")
+def recordCallback(indata, frames, time, status):
+    if status:
+        print(status, file=sys.stderr)
+    q.put(bytes(indata))
+    
+# build the model and recognizer objects.
+print("===> Build the model and recognizer objects.  This will take a few minutes.")
+model = Model("vosk-model-small-en-us-0.15")
+recognizer = KaldiRecognizer(model, samplerate)
+recognizer.SetWords(False)
 
-    except KeyboardInterrupt:
-        print('===> Finished Recording')
-    except Exception as e:
-        print(str(e))
+print("===> Begin recording. Press Ctrl+C to stop the recording ")
+try:
+    with sd.RawInputStream(dtype='int16',
+                           channels=1,
+
+                           callback=recordCallback):
+        while True:
+            data = q.get()        
+            if recognizer.AcceptWaveform(data):
+                recognizerResult = recognizer.Result()
+                # convert the recognizerResult string into a dictionary  
+                resultDict = json.loads(recognizerResult)
+                if resultDict.get("text", "") != "":
+                    print(recognizerResult)
+                else:
+                    print("no input sound")
+
+except KeyboardInterrupt:
+    print('===> Finished Recording')
+except Exception as e:
+    print(str(e))
