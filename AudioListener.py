3a7414a239172eb6f15c0a5d6d97407b57e0b668
Jack Gardner <shortfilmsbyjack@aol.com>
2023-12-01 20:40:16 -0800

    Revert "deleted unnecesary code in audiolistener"
    This reverts commit 98a50c7eaf3a4390cbbd09e3d4bbb9132b9e8b8e.

--- AudioListener.py
+++ AudioListener.py
@@ -7,6 +7,34 @@ from time import sleep
 import wave
 
 
+# A class mocking actual functionality of audiolistening, by returning 
+def listen_for_audio(plane_id, audiobitQ, audioComIn, audioComOut):
+    listening = True
+    nameHeard = False
+    
+    # TODO : Obstantiate 
+
+    while(True):
+        
+        if nameHeard:
+        
+            # TODO : Replace sleep function with listening function
+            sleep(5)
+            # while audioNotQuiet
+
+            # TODO : Replace this line with the audio file parsed from above
+            audio = wave.open("testAudio.wav", 'rb')
+            
+            audiobitQ.put(audio)
+            nameHeard = False
+        else:
+
+            sleep(5)
+            nameHeard = True
+            # TODO : listen for name to be called
+
+
+
 
 '''This script processes audio input from the microphone and displays the transcribed text.'''
 # get the samplerate - this is needed by the Kaldi recognizer
