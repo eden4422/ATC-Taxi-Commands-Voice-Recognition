637374b9a574beb64c925fbfd14366f80a558ff7
cromeros <cromerosanchez@wsu.edu'clear>
2023-12-02 20:41:28 -0800

    Working making wav files audio listener working somewhat

--- AudioListening.py
+++ AudioListening.py
@@ -7,89 +7,73 @@ from time import sleep
 import wave
 import multiprocessing
 import queue
-import io
-import os
-import tempfile
 
 # A class mocking actual functionality of audiolistening, by returning 
 def listen_for_audio(plane_id, audiobitQ, audioComIn, audioComOut):
-    print("started listening")
-    '''This script processes audio input from the microphone and displays the transcribed text.'''
+
+    flight_IDs = ["delta one two three", "united six seven eight"]
+    fileNum = 0
     # get the samplerate - this is needed by the Kaldi recognizer
     device_info = sd.query_devices(sd.default.device[0], 'input')
     samplerate = int(device_info['default_samplerate'])
 
     # display the default input device
-    print("===> Initial Default Device Number:{} Description: {}".format(sd.default.device[0], device_info),flush=True)
+    print("===> Initial Default Device Number:{} Description: {}".format(sd.default.device[0], device_info))
 
     # setup queue and callback function
-    q = multiprocessing.Queue()
+    q = queue.Queue()
 
     def recordCallback(indata, frames, time, status):
         if status:
             print(status, file=sys.stderr)
         q.put(bytes(indata))
-    
+
     # build the model and recognizer objects.
     print("===> Build the model and recognizer objects.  This will take a few minutes.")
     model = Model("vosk-model-small-en-us-0.15")
     recognizer = KaldiRecognizer(model, samplerate)
     recognizer.SetWords(False)
 
+    desired_duration = 40  #10 = 5 seconds
+    frame_limit = int(samplerate * desired_duration)
+
+
     print("===> Begin recording. Press Ctrl+C to stop the recording ")
-
-
     try:
-        with sd.RawInputStream(dtype='int16', channels=1, callback=recordCallback):
+        recording_data = b''  # Accumulate audio data
+
+        with sd.RawInputStream(dtype='int16',
+                            channels=1,
+                            callback=recordCallback):
             while True:
-                audio_data = q.get()
-                if recognizer.AcceptWaveform(audio_data):
+                data = q.get()
+                recording_data += data  # Accumulate audio data
+                if recognizer.AcceptWaveform(data):
                     recognizerResult = recognizer.Result()
                     # convert the recognizerResult string into a dictionary
                     resultDict = json.loads(recognizerResult)
                     resultText: str = resultDict["text"]
                     print(resultText)
-                    if resultText.startswith("banana"):
-                        print(resultText)
-                        print(audio_data)
-                        # Save audio as WAV file
-                        save_wav_filename = os.path.join("AudioRecordings", f"{plane_id}_recording.wav")
+                    for ID in flight_IDs:
+                        if ID in resultText:
+                            print(resultText)
 
-                        os.makedirs(os.path.dirname(save_wav_filename), exist_ok=True)
+                            # Save the accumulated audio data to a WAV file
+                            with wave.open(f'output{fileNum}.wav', 'w') as wf:
+                                wf.setnchannels(1)
+                                wf.setsampwidth(2)
+                                wf.setframerate(samplerate)
+                                wf.writeframes(recording_data[-frame_limit:])
+                                fileNum += 1
+                                if fileNum == 100:
+                                    fileNum = 0
+                                
+                            recording_data = b''  # Reset accumulated audio data
 
-                        with wave.open(save_wav_filename, 'wb') as wav_file:
-                            wav_file.setnchannels(1)  # Mono
-                            wav_file.setsampwidth(2)  # 2 bytes per sample
-                            wav_file.setframerate(samplerate)
-                            wav_file.writeframes(audio_data)
-
-                        audiobitQ.put(save_wav_filename)
-                    else:
-                        print("no input sound")
+                        else:
+                            print("no input sound")
 
     except KeyboardInterrupt:
         print('===> Finished Recording')
     except Exception as e:
         print(str(e))
-
-
-
-def save_as_wav(audio_data, samplerate):
-    # Create a temporary WAV file
-    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_wav_file:
-        temp_wav_filename = temp_wav_file.name
-
-        with wave.open(temp_wav_filename, 'wb') as wave_file:
-            wave_file.setnchannels(1)  # Mono
-            wave_file.setsampwidth(2)  # 2 bytes per sample
-            wave_file.setframerate(samplerate)
-            wave_file.writeframes(audio_data)
-
-        # Read the contents of the temporary file into a buffer
-        with open(temp_wav_filename, 'rb') as temp_file:
-            wav_buffer = temp_file.read()
-
-    # Delete the temporary WAV file
-    os.remove(temp_wav_filename)
-
-    return wav_buffer
