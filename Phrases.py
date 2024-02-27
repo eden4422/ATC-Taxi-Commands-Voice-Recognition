4d7db18c0284424c1e74dd2fc70d080c95fc93ef
Jack Gardner <shortfilmsbyjack@aol.com>
2024-02-26 18:27:39 -0800

    Added sentence class
    added sentence class which stores a list of word classes and returns a colored string based on a preset range of certainty values

--- Phrases.py
+++ Phrases.py
@@ -2,3 +2,26 @@ class Word:
     def __init__(self, word, certainty):
         self.word = word
         self.certainty = certainty
+
+class Sentence:
+    def __init__(self):
+        self.words = []
+
+    def add_word(self, word):
+        if not isinstance(word, Word):
+            raise ValueError("Input must be an instance of Word")
+        self.words.append(word)
+
+    def to_string(self, certainty_ranges):
+        colored_words = []
+        for word in self.words:
+            color = self.get_color(word.certainty, certainty_ranges)
+            colored_word = f"\033[{color}m{word.word}\033[0m"
+            colored_words.append(colored_word)
+        return ' '.join(colored_words)
+
+    def get_color(self, certainty, certainty_ranges):
+        for range_start, range_end, color_code in certainty_ranges:
+            if range_start <= certainty <= range_end:
+                return color_code
+        return "0"  # Default color code
